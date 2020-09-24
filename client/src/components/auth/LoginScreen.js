d5d299acf04c9543438907bc94b0b74b604db495
irojas14 <ignacio.rojas.14@sansano.usm.cl>
2020-09-23 21:07:35 -0300

    Arreglo final 1.0 del login

--- client/src/components/auth/LoginScreen.js
+++ client/src/components/auth/LoginScreen.js
@@ -32,7 +32,7 @@ export const LoginScreen = () => {
             <div style={{height: "100vh", width: "100vw", backgroundColor: "#da0333"}}>
             <div className="m-4">
                 <hr />
-                <div className="login" onSubmit={ handleSubmitForm }>
+                <form  style={{height: "450px", width: "400px", borderRadius: "15px 15px 15px 15px"}} onSubmit={ handleSubmitForm }>
                     <div className="form-group">
                     <img className="icon_u_assist" src={logo} alt="U-Assist"></img>
                     <h2 className="text-tittle-formulary"> Iniciar Sesión </h2>
@@ -68,7 +68,7 @@ export const LoginScreen = () => {
                     <div className="text-center">
                         <p> ¿No tienes una cuenta? <Link to="/register"> Crea una nueva </Link> </p>
                     </div>
-                </div>
+                </form>
                 <hr />
             </div>    
             </div>
